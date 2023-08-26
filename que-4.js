89067b936825f7c00ba930142daa1a7b768a9d0b
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-08-26 11:19:05 +0530

    solved que-4

--- que-4.js
+++ que-4.js
@@ -1,3 +1,15 @@
+/* que-4)-->>
+Write a function createCounter. It should accept an initial integer init. It should return an object with three 
+functions.
+
+The three functions are:
+
+increment() increases the current value by 1 and then returns it.
+decrement() reduces the current value by 1 and then returns it.
+reset() sets the current value to init and then returns it.
+*/
+
+//sol ->
 const createCounter = (init) => {
   let newNum = init;
   const increment = () => ++newNum;
