613075487e255db361a194698c9f9004e1774ee2
Faizan Siddiqui <faizansiddiquirmr@gmail.com>
2023-10-13 19:13:44 +0530

    ds question solved

---
+++ que-39.js
@@ -0,0 +1,16 @@
+/*
+fabonacci series using Big 0 complexity
+*/
+
+const fabonacci= (n) =>{
+    const fabo = [0,1]
+    for(i=2;i<n;i++){
+        fabo[i] = fabo[i-1]+fabo[i-2]
+    }
+    return fabo
+
+}
+
+console.log(fabonacci(2))
+console.log(fabonacci(4))
+console.log(fabonacci(7))
